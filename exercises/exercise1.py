914dfa6184e7cb520c908ca897b4074ec3493f86
hermankraus <95387478+hermankraus@users.noreply.github.com>
2022-03-31 19:36:52 -0300

    Ejercicio1 resuelto

--- exercises/exercise1.py
+++ exercises/exercise1.py
@@ -10,6 +10,8 @@ lado_cuadrado = 5
 
 # COMPLETAR - INICIO
 
+area_cuadrado= lado_cuadrado*lado_cuadrado
+
 # COMPLETAR - FIN
 
 assert area_cuadrado == 25
@@ -23,6 +25,8 @@ lado_cuadrado = 5
 
 # COMPLETAR - INICIO
 
+area_cuadrado= lado_cuadrado**2
+
 # COMPLETAR - FIN
 
 assert area_cuadrado == 25
@@ -35,6 +39,8 @@ Re-Escribir usando la función pow.
 lado_cuadrado = 5
 
 # COMPLETAR - INICIO
+
+pow(5 , 2)
 
 # COMPLETAR - FIN
 
@@ -51,6 +57,8 @@ presupuesto_disponible = 10
 
 # COMPLETAR - INICIO
 
+cantidad_a_comprar= precio//presupuesto_disponible
+
 # COMPLETAR - FIN
 
 assert cantidad_a_comprar == 2
@@ -65,6 +73,8 @@ numero_incalculable = 2 ** 54 - 1
 
 # COMPLETAR - INICIO
 
+es_divisible_por_siete= numero_incalculable % 7
+
 # COMPLETAR - FIN
 
-assert es_divisible_por_siete
+assert es_divisible_por_siete==0
