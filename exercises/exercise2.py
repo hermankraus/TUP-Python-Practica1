b4806658db910b223285e003995e49686688e71a
hermankraus <95387478+hermankraus@users.noreply.github.com>
2022-03-31 20:12:49 -0300

    Ejercicio2 resuelto

--- exercises/exercise2.py
+++ exercises/exercise2.py
@@ -6,10 +6,15 @@ Construir una expresión lógica que use TODAS las variables y cuyo resultado se
 True si al menos una de las variables es True.
 """
 
+from pickle import TRUE
+
+
 esta_lloviendo = True
 riego_activado = True
 
 # COMPLETAR - INICIO
+ 
+piso_mojado = esta_lloviendo or riego_activado 
 
 # COMPLETAR - FIN
 
@@ -27,6 +32,8 @@ area_cuadrado = pow(lado_cuadrado, 2)
 
 # COMPLETAR - INICIO
 
+area_mayor_a_cinco = not area_cuadrado < 5
+
 # COMPLETAR - FIN
 
 assert area_mayor_a_cinco
@@ -41,6 +48,8 @@ numero_1 = 49
 numero_2 = 50
 
 # COMPLETAR - INICIO
+
+resultado = ( numero_1 % 7 == 0) and ( numero_2 % 7 == 1)
 
 # COMPLETAR - FIN
 
@@ -61,6 +70,9 @@ variable_05 = 100
 
 # COMPLETAR - INICIO
 
+resultado= variable_03 or not variable_01 or not variable_02 or not variable_04 or not variable_05 
+print(resultado)
 # COMPLETAR - FIN
 
 assert resultado == 80
+
---
+++ exercises/tempCodeRunnerFile.py
@@ -0,0 +1,10 @@
+variable_01 = False
+variable_02 = True
+variable_03 = 80
+variable_04 = "90"
+variable_05 = 100
+
+# COMPLETAR - INICIO
+
+resultado= variable_03 or not variable_01 or not variable_02 or not variable_04 or not variable_05 
+print(resultado)
