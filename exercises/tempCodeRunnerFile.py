f1f9bb84a047e086a240c229ba8094c3cb1588a9
bortoli94 <gbortoli.rosario@hotmail.com>
2022-04-04 17:59:53 -0300

    Ejercicio 4 y 5 completos Gason

---
+++ exercises/tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+variable_01 = "Le pagué "
+variable_02 = 2
+variable_03 = " pesos que le debía a Ezequiel, me faltan $"
+variable_04 = 4
+
+# COMPLETAR - INICIO
+strings_concatenados = f"{variable_01}{variable_02}{variable_03}{variable_04}"
+# COMPLETAR - FIN
+print(strings_concatenados)
+
+assert strings_concatenados == "Le pagué 2 pesos que le debía a Ezequiel, me faltan $4"
